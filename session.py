ead18186a486193d7fe19b1adb2790baf462360b
biduz <biduzeez@gmail.com>
2016-04-09 03:32:07 -0300

    Fix error in session.py
    Can't return None when getting token or hmac will
    not work

--- session.py
+++ session.py
@@ -4,18 +4,20 @@ import random
 session = {}
 
 def new_session(user):
-	session[user.key().id()] = {}
-	session[user.key().id()]['token'] = make_salt()
+    session[user.key().id()] = {}
+    session[user.key().id()]['token'] = make_salt()
 
 def delete_session(user):
-	del session[user.key().id()]
+    del session[user.key().id()]
 
 def get_session_token(user_id):
-	try:
-		return session[user_id]['token']
-	except:
-		return None
+    try:
+        return session[user_id]['token']
+    except:
+        import logging
+        logging.error('Failed importing token. user_id = %s' % user_id)
+        return ' ' # hmac will not be happy if return None here
 
 def make_salt(n=20):
-	chars = string.digits + string.letters
-	return ''.join(random.choice(chars) for x in range(n))
+    chars = string.digits + string.letters
+    return ''.join(random.choice(chars) for x in range(n))
