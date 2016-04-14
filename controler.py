246904ebad0448d4fd0f1a3d6464ad6ef714a56f
biduz <biduzeez@gmail.com>
2016-04-14 19:17:46 -0300

    Modularize some functions
    The functions to validate the login and signup forms that were in
    controler.py, are now in stuff.py.

--- controler.py
+++ controler.py
@@ -1,23 +1,10 @@
 from main_handler import MainHandler
 import model
 import stuff
-import re
 
 class Index(MainHandler):
     def get(self):
         self.render('index.html')
-
-def available_username(username):
-    return not model.get_user_by_name(username)
-
-def valid_username(username):
-    return username and re.match('^[\S]+$', username)
-
-def valid_password(password, verify):
-    return password and password == verify
-
-def valid_email(email):
-    return not email or re.match('^[\S]+@[\S]+\.[\S]+$', email)
 
 class Signup(MainHandler):
     def get(self):
@@ -28,27 +15,8 @@ class Signup(MainHandler):
         password = self.request.get('password')
         verify = self.request.get('verify')
         email = self.request.get('email')
-
-        have_error = False
-        params = {'username_value': username,
-                  'email_value': email}
-        if not valid_username(username):
-            have_error = True
-            params['username_error'] = "That's not a valid username."
-        elif not available_username(username):
-            have_error = True
-            params['username_error'] = "That username is not available"
-        if not password:
-            have_error = True
-            params['password_error'] = "That's not a valid password."   
-        elif not valid_password(password, verify):
-            have_error = True
-            params['verify_error'] = "Sorry, the passwords didn't match."
-        if not valid_email(email):
-            have_error = True
-            params['email_error'] = "That's not a valid e-mail."
-        
-        if have_error:
+        error, params = stuff.valid_signup(username, password, verify, email)
+        if error:
             self.render('signup.html', **params)       
         else:
             password = stuff.make_pw_hash(password)
@@ -63,18 +31,12 @@ class Login(MainHandler):
     def post(self):
         username = self.request.get('username')
         password = self.request.get('password')
-        
-        user = model.get_user_by_name(username)
-        if user:
-            if stuff.valid_pw(password, user.password):
-                self.login(user)
-                self.redirect('/')
-            else:
-                error = 'Invalid Password'
-                self.render('login.html', username = username, error = error)    
+        error, params, user =  stuff.valid_login(username, password)
+        if not error:
+            self.login(user)
+            self.redirect('/')   
         else:
-            error = 'Invalid User'
-            self.render('login.html', username = username, error = error)
+            self.render('login.html', **params)
 
 class Logout(MainHandler):
     def get(self):
--- stuff.py
+++ stuff.py
@@ -2,6 +2,8 @@ from passlib.hash import sha512_crypt
 import session
 import hmac
 import hashlib
+import re
+import model
 
 #Password stuff
 def make_pw_hash(pw):
@@ -33,3 +35,51 @@ def escape_html(html):
     escaped = html.replace('<script>', '&lt;script&gt;')
     escaped = escaped.replace('</script>', '&lt;/script&gt;')
     return escaped
+
+def valid_signup(username, password, verify, email):
+    have_error = False
+    params = {'username_value': username,
+              'email_value': email}
+    if not valid_username(username):
+        have_error = True
+        params['username_error'] = "That's not a valid username."
+    elif not available_username(username):
+        have_error = True
+        params['username_error'] = "That username is not available"
+    if not password:
+        have_error = True
+        params['password_error'] = "That's not a valid password."   
+    elif not valid_password(password, verify):
+        have_error = True
+        params['verify_error'] = "Sorry, the passwords didn't match."
+    if not valid_email(email):
+        have_error = True
+        params['email_error'] = "That's not a valid e-mail."
+    return have_error, params
+
+def valid_username(username):
+    return username and re.match('^[\S]+$', username)
+
+def available_username(username):
+    return not model.get_user_by_name(username)
+
+def valid_password(password, verify):
+    return password and password == verify
+
+def valid_email(email):
+    return not email or re.match('^[\S]+@[\S]+\.[\S]+$', email)
+
+def valid_login(username, password):
+    have_error = False
+    params = {'username': username}
+    user = model.get_user_by_name(username)
+    if user:
+        if valid_pw(password, user.password):
+            return have_error, params, user
+        else:
+            have_error = True
+            params['error'] = 'Invalid Password'
+    else:
+        have_error = True
+        params['error'] = 'Invalid User'
+    return have_error, params, user
